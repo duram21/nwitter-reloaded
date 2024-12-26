27e4eabf10ee92d44363557e85cea80fa9f9d346
[duram21] <[cubeenjoy@naver.com]>
2024-12-26 22:26:49 +0900

    try:

---
+++ src/firebase.ts
@@ -0,0 +1,13 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAgmwg6p43CyvbrqJry0udFpBAt_n3NwQs",
+  authDomain: "nwitter-reloaded-1105a.firebaseapp.com",
+  projectId: "nwitter-reloaded-1105a",
+  storageBucket: "nwitter-reloaded-1105a.firebasestorage.app",
+  messagingSenderId: "98979528433",
+  appId: "1:98979528433:web:66ddca6433ca9dfa93b350"
+};
+
+const app = initializeApp(firebaseConfig);
